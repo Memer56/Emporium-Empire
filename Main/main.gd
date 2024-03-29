805af97d245d0cfcf8bf846fd3438b06e8db3427
Memer56 <96455752+Memer56@users.noreply.github.com>
2024-03-29 22:29:18 +0000

    Added new save and load functions main.gd

--- Main/main.gd
+++ Main/main.gd
@@ -4,6 +4,7 @@ extends Node3D
 const BAGUETTE = preload("res://Items/baguette.tscn")
 const PEANUT_BUTTER_JAR = preload("res://Items/peanut_butter_jar.tscn")
 const CASH = preload("res://Cash/cash.tscn")
+const SHELF = preload("res://Shelf/shelf.tscn")
 
 @onready var navigation_region = $NavigationRegion3D
 @onready var player = $Player
@@ -11,6 +12,8 @@ const CASH = preload("res://Cash/cash.tscn")
 @onready var num : int
 
 var nav_rebake_finished = true
+var save : SaveGame
+var item_data 
 
 
 func _ready():
@@ -18,8 +21,13 @@ func _ready():
 	EventBus.spawn_picked_items.connect(spawn_picked_items)
 	EventBus.unlock_player_look_axis.connect(unlock_player_look_axis)
 	EventBus.spawn_money.connect(spawn_money)
+	if EventBus.loaded_game_file_from_main_menu:
+		_on_pause_menu_load_game(EventBus.id)
+	if spawn_things:
+		spawn_thing()
 	#for items in get_tree().get_nodes_in_group("items"):
 		#print(str(items.name, items.global_position))
+
 
 func _process(_delta):
 	if get_node_or_null("NavigationRegion3D/Checkout"):
@@ -66,3 +74,62 @@ func unlock_player_look_axis():
 func _on_navigation_region_3d_bake_finished():
 	nav_rebake_finished = true
 
+
+func _on_pause_menu_save_game(id : String):
+	save = SaveGame.new()
+	save.player_pos = player.global_position
+	for item in get_tree().get_nodes_in_group("save"):
+		var object_info = []
+		var positions = item.global_position
+		var item_id = item.id
+		var object_rotation = item.global_rotation_degrees
+		object_info.append(item_id)
+		object_info.append(positions)
+		object_info.append(object_rotation)
+		save.item_and_object_pos.append(object_info)
+	save.write_savegame(id)
+
+
+func _on_pause_menu_load_game(id : String):
+	if SaveGame.save_exists(id) == false:
+		push_error("No Save File Was Found")
+		return
+	# reload scene before executing this or it causes duplication glitch
+	save = SaveGame.load_savegame(id)
+	player.global_position = save.player_pos
+	item_data = save.item_and_object_pos
+	for index in item_data.size():
+		var data = item_data[index]
+		var item_id = data[0]
+		var pos = data[1]
+		var rot = data[2]
+		var IGNORE = null
+		var item_name : PackedScene
+		match item_id:
+			1:
+				pass
+				#item_name = PEANUT_BUTTER_JAR
+				#var itemName = item_name.instantiate()
+				#add_child(itemName)
+				#itemName.global_position = pos
+			2:
+				item_name = BAGUETTE
+				var itemName = item_name.instantiate()
+				add_child(itemName)
+				itemName.global_position = pos
+				itemName.global_rotation_degrees = rot
+			3:
+				item_name = PEANUT_BUTTER_JAR
+				var itemName = item_name.instantiate()
+				add_child(itemName)
+				itemName.global_position = pos
+				itemName.global_rotation_degrees = rot
+			4:
+				pass
+			5:
+				item_name = SHELF
+				var itemName = item_name.instantiate()
+				add_child(itemName)
+				itemName.global_position = pos
+				itemName.global_rotation_degrees = rot
+				itemName.object_white()
