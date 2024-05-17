3e5fcc2a3e53894fe7b0525d53ab7cc9a77331b3
Memer56 <96455752+Memer56@users.noreply.github.com>
2024-05-17 23:10:27 +0100

    Fixed cheat bug cheat_manager.gd

--- Cheat Console/cheat_manager.gd
+++ Cheat Console/cheat_manager.gd
@@ -1,3 +1,11 @@
 extends Node
 
-signal change_state(new_state : int)
+func change_player_state(new_state : String):
+	EventBus.cheat_command_entered = true
+	match new_state:
+		"FLY":
+			EventBus.current_player_state = EventBus.player_state.FLY
+			EventBus.cheat_command_entered = true
+		"NORMAL":
+			EventBus.current_player_state = EventBus.player_state.NORMAL
+			EventBus.cheat_command_entered = false
