a8dd0d3ad1ba8afd402ec4d36eeb0ec26110f50d
Memer56 <96455752+Memer56@users.noreply.github.com>
2024-08-02 21:18:54 +0100

    Add files via upload

--- UI/image_button.gd
+++ UI/image_button.gd
@@ -3,14 +3,31 @@ extends Control
 @onready var texture_rect = $Button/TextureRect
 @onready var price_tag = $Button/PriceTag
 @onready var object_name = $Button/ObjectName
-@export var Price : String
+@export var Price : int
 @export var Name : String
+@export var Object_Image : Texture
 
+var new_colour : Color
+var default_colour := Color.WHITE
+var colour_was_chosen := false
 
 func _ready():
 	price_tag.text = str(Price)
 	object_name.text = str(Name)
+	texture_rect.texture = Object_Image
+
+func remove_self():
+	queue_free()
 
 
 func _on_button_pressed():
-	EventBus.image_button_pressed.emit(Name)
+	if colour_was_chosen:
+		EventBus.image_button_pressed.emit(Name, new_colour)
+		colour_was_chosen = false
+	else:
+		EventBus.image_button_pressed.emit(Name, default_colour)
+
+
+func _on_color_picker_button_color_changed(color):
+	colour_was_chosen = true
+	new_colour = color
