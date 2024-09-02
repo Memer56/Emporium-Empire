f37b2f16ca968368bfff2fc06c75f49e9e94fd55
Memer56 <96455752+Memer56@users.noreply.github.com>
2024-09-02 03:25:41 +0100

    Update event_bus.gd

--- EventBus/event_bus.gd
+++ EventBus/event_bus.gd
@@ -2,17 +2,17 @@ extends Node
 
 signal remove_spawnable
 signal display_object(object : PackedScene)
-signal object_white
 signal recalculate_nav_region
-signal toggle_door
 signal freeze_npc
 signal send_picked_item_data(item_names : Array, placement_position : Vector3)
 signal unlock_player_look_axis
 signal clicked_on_cash_register
-signal image_button_pressed(object_name : String)
+signal image_button_pressed(object_name : String, new_colour : Color)
 signal player_drop_object
 signal progress_in_queue
 signal reconnect_signal
+signal send_confirmed_order(basket : Array)
+signal change_shelf_points_true_false_array_value(index : int, make_false : bool)
 
 enum state {
 	PLAY,
@@ -32,7 +32,11 @@ var player_pos = Vector3.ZERO
 var checkouts_exist_in_scene := false
 var num_of_items_in_world = 0
 var id : String
-var loaded_game_file_from_main_menu := false
-var is_shop_open := true
+var loaded_game_file := false
+var is_shop_open := false
 var npc_heading_to_checkout := false
 var cheat_command_entered := false
+var player_is_in_house := false
+var npc_spawn_points : Array
+var player_level : int
+var last_item_placed_on_shelf : String
